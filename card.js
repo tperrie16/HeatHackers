d7f7046bc7a3aedef423c67d894cb067a326b2c7
tperrie16 <tylerperrie16@gmail.com>
2025-05-28 23:48:13 -0300

    delete card.js

--- card.js
+++ card.js
@@ -1,2 +1,1 @@
-export const Card = ({ children, className }) => <div className={`bg-white border p-4 ${className}`}>{children}</div>;
-export const CardContent = ({ children, className }) => <div className={className}>{children}</div>;
+
