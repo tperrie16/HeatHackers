606b73f909a65081d82b4f75b61931d912d68fb4
tperrie16 <tylerperrie16@gmail.com>
2025-05-28 23:53:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
 import Link from "next/link";
 
 export default function HeatHackers() {
