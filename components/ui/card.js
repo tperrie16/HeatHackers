5d20f449e33bd4f5f2c1797f4cd3afa3558e781d
tperrie16 <tylerperrie16@gmail.com>
2025-05-28 23:16:34 -0300

    Update card.js

--- components/ui/card.js
+++ components/ui/card.js
@@ -1,1 +1,9 @@
+export const Card = ({ children, className }) => (
+  <div className={`bg-white border p-4 rounded-lg shadow-md ${className}`}>
+    {children}
+  </div>
+);
 
+export const CardContent = ({ children, className }) => (
+  <div className={className}>{children}</div>
+);
